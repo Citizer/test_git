3e7102eae3e93a3cbb41730ecb6ee1980e6aa9d0
citizer <potopahin98@mail.ru>
2018-02-08 06:13:14 +0600

    Вторая попытка

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,6 @@ package sample;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello, Kamo!");
+        System.out.println("Hello, World!");
     }
 }
